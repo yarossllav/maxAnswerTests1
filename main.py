25ff8a24aa7870b21901e3725b9bfb3a24a2d31a
max <maxvin13a@gmail.com>
2024-11-29 17:54:21 +0100

    test ende

---
+++ main.py
@@ -0,0 +1,28 @@
+'1'
+number = int(input("number1:"))
+number2 = int(input("number2:"))
+number3 = int(input("number3:"))
+print(number + number2 - number3)
+
+'2'
+name = (input("name:"))
+age = int(input("age:"))
+hobi = (input("hobi:"))
+print('My name is', name, 'I am', age, 'and my hobby is', hobi)
+
+'3'
+number = int(input("number1:"))
+number2 = int(input("number2:"))
+print(number, '+', number2, '=', number + number2 )
+
+'4'
+teim = int(input('teim:'))
+spid = int(input('spid:'))
+print(teim * spid)
+
+'5'
+n = int(input('esteemed:'))
+tips = 10
+pipel = 3
+d = n * tips
+print(d/pipel)
